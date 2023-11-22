f6b973e970bb2185ebd415bcffd9b1b7d53d3354
saad-13 <148326859+saad-13@users.noreply.github.com>
2023-11-22 16:03:46 +0000

    added nav bar to login page

--- login.php
+++ login.php
@@ -4,9 +4,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="login.css">
 </head>
 <body>
+
+        <header>
+            <div id="logo-container">
+                <!-- logo image -->
+                <img id="logo" src="Images/Logo-no-bg.png" alt="Logo">
+                <h1 id="nav-bar-text">ACE GEAR</h1>
+            </div>
+            <div id="search-container">
+                <input type="text" id="search-bar" placeholder="Search...">
+                <button id="search-button">Search</button>
+            </div>
+            <nav>
+                <a href="index.html">Home</a>
+                <a href="products.html">Products</a>
+                <a href="#">About</a>
+                <a href="contact.html">Contact</a>
+                <a href="login.php">Login</a>
+            </nav>
+            <div id="cart-container">
+                <!-- cart icon image -->
+                <img id="cart-icon" src="Images/cart-no-bg.png" alt="Cart">
+                <span id="cart-count">0</span>
+            </div>
+        </header>
+
+
+
     <div class="login-container">
         <h2>Login</h2>
         <form action="login.php" method="post">
@@ -19,5 +46,7 @@
             <input type="submit" value="Login">
         </form>
     </div>
+
+    
 </body>
 </html>
