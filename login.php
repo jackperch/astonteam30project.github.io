f8bb00ca91208531bf4f0e5497aac3a25773d811
saad-13 <148326859+saad-13@users.noreply.github.com>
2023-11-27 17:12:07 +0000

    content container added to login page

--- login.php
+++ login.php
@@ -90,22 +90,22 @@
         </header>
 
 
+    <div class="content-container">
+        <div class="login-container">
+            <h2>Login</h2>
+            <form action="login.php" method="post">
+                <label for="username">Username:</label>
+                <input type="text" id="username" name="username" required>
 
-    <div class="login-container">
-        <h2>Login</h2>
-        <form action="login.php" method="post">
-            <label for="username">Username:</label>
-            <input type="text" id="username" name="username" required>
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" required>
 
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password" required>
-
-            <input type="submit" value="Login">
-            <input type="hidden" name="loginsubmitted" value="TRUE" />
-        </form>
-        <a href="signup.php" class="signup-button">Dont have an account? Sign Up here</a>
+                <input type="submit" value="Login">
+                <input type="hidden" name="loginsubmitted" value="TRUE" />
+            </form>
+            <a href="signup.php" class="signup-button">Dont have an account? Sign Up here</a>
+        </div>
     </div>
-
 
 
 
