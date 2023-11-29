69e2ae3c377fb249a981308a5ff6fd65a167597b
tharrm <131722887+tharrm@users.noreply.github.com>
2023-11-29 12:05:17 +0000

    changes made

--- connectionDB.php
+++ connectionDB.php
@@ -3,10 +3,10 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "acegear";
+$dbame = "acegear";
 
 try {
-	$db = new PDO("mysql:dbname=$db_name;host=$db_host", $username, $password); 
+	$db = new PDO("mysql:dbname=$dbname;host=$servername", $username, $password); 
 	#$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $ex) {
 	echo("Failed to connect to the database.<br>");
