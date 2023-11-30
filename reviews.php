6a60288b334528418cfd39be6f6c139c4dd6a479
saad-13 <148326859+saad-13@users.noreply.github.com>
2023-11-30 14:22:04 +0000

    added basic code to members-blog and reviews pages

--- reviews.php
+++ reviews.php
@@ -1,22 +1,3 @@
-<?php
-// Start the session to access session variables
-session_start();
-
-// Check if the user is logged in
-if (!isset($_SESSION['user_id'])) {
-    // If not logged in, redirect to the login page
-    header("Location: login.php");
-    exit();
-}
-
-// If logged in, proceed with displaying the page
-
-// configuration or database connection files
-
-// Rest of blog page HTML and PHP code here
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
     <head>
