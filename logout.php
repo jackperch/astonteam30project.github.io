5c7eb09de656986fdd2bcc8996b4903f641c0aac
saad-13 <148326859+saad-13@users.noreply.github.com>
2023-11-30 21:17:44 +0000

    added new cart css page and basic html code for logout.php

--- logout.php
+++ logout.php
@@ -7,12 +7,67 @@ session_destroy();
 
 <html>
     <head>
+        
+        <title>ACE GEAR</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="CSS/styles.css">
+        <style>
+            @import url('https://fonts.googleapis.com/css2?family=Sono&display=swap');
+        </style>
 
     </head>
     <body>
-        <div id="log-out">
-            <H1> Logged out now! </H1> 
-                <p><a href="index.html">Home Page</a> </p>
+
+        <header>
+            <div id="logo-container">
+                <!-- logo image -->
+                <img id="logo" src="Images/Logo-no-bg.png" alt="Logo">
+                <h1 id="nav-bar-text">ACE GEAR</h1>
             </div>
+            <div id="search-container">
+                <input type="text" id="search-bar" placeholder="Search...">
+                <button id="search-button">Search</button>
+            </div>
+            <nav>
+                <a href="index.html">Home</a>
+                <a href="products.php">Products</a>
+                <a href="about.html">About</a>
+                <a href="members-blog.php">Blog</a>
+                <a href="contact.html">Contact</a>
+                <a href="login.php">Login</a>
+            </nav>
+            <div id="cart-container">
+                <!-- cart icon image with link to cart page -->
+                <a href="cart.php">
+                    <img id="cart-icon" src="Images/cart-no-bg.png" alt="Cart">
+                    <span id="cart-count">0</span>
+                </a>
+            </div>
+        </header>
+
+
+
+        <div id="log-out">
+            <h1> Logged out now! </h1> 
+                <p><a href="index.html">Click here to return to Home Page</a> </p>
+            </div>
+
+
+
+
+        <footer>
+            <div class="footer-container">
+                <div class="footer-links">
+                    <a href="reviews.php">Reviews</a>
+                    <a href="contact.html">Contact Us</a>
+                    <a href="about.html">About Us</a>
+                    <a href="privacy-policy.html">Privacy Policy</a>
+                </div>
+            </div>
+        </footer>
+
+
+
     </body>
 </html>
