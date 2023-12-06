e85af0f511f08b65441107df545dc6f578a29f39
saad-13 <148326859+saad-13@users.noreply.github.com>
2023-12-06 12:31:41 +0000

    starting to add css to products page

--- products.php
+++ products.php
@@ -1,3 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>ACE GEAR</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="CSS/products.css">
+        <style>
+            @import url('https://fonts.googleapis.com/css2?family=Sono&display=swap');
+        </style>
+        <script src="/js/main.js"></script>
+    </head>
+<body>
+    
+    <header>
+        <div id="logo-container">
+            <!-- logo image -->
+            <img id="logo" src="Images/Logo-no-bg.png" alt="Logo">
+            <h1 id="nav-bar-text">ACE GEAR</h1>
+        </div>
+        <div id="search-container">
+            <input type="text" id="search-bar" placeholder="Search...">
+            <button id="search-button">Search</button>
+        </div>
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="products.php">Products</a>
+            <a href="about.html">About</a>
+            <a href="contact.html">Contact</a>
+
+
+            <?php 
+                session_start();
+                if (isset($_SESSION['username'])) {
+                    echo "<a href='logout.php'>Logout</a>";
+                } else {
+                    echo "<a href='account.php'>Account</a>";
+                    echo "<a href='login.php'>Login</a>";
+                    
+                }
+                ?>
+        </nav>
+        <div id="cart-container">
+            <!-- cart icon image with link to cart page -->
+            <a href="cart.php">
+                <img id="cart-icon" src="Images/cart-no-bg.png" alt="Cart">
+                <span id="cart-count">0</span>
+            </a>
+        </div>
+    </header>
+
 <?php
 
 require_once("connectionDB.php");
@@ -51,56 +102,7 @@ $allOfTheProducts = fetchProducts();
 
     
 
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <title>ACE GEAR</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta charset="UTF-8">
-        <link rel="stylesheet" href="CSS/styles.css">
-        <style>
-            @import url('https://fonts.googleapis.com/css2?family=Sono&display=swap');
-        </style>
-        <script src="/js/main.js"></script>
-    </head>
-<body>
-    
-    <header>
-        <div id="logo-container">
-            <!-- logo image -->
-            <img id="logo" src="Images/Logo-no-bg.png" alt="Logo">
-            <h1 id="nav-bar-text">ACE GEAR</h1>
-        </div>
-        <div id="search-container">
-            <input type="text" id="search-bar" placeholder="Search...">
-            <button id="search-button">Search</button>
-        </div>
-        <nav>
-            <a href="index.php">Home</a>
-            <a href="products.php">Products</a>
-            <a href="about.html">About</a>
-            <a href="contact.html">Contact</a>
 
-
-            <?php 
-                session_start();
-                if (isset($_SESSION['username'])) {
-                    echo "<a href='logout.php'>Logout</a>";
-                } else {
-                    echo "<a href='account.php'>Account</a>";
-                    echo "<a href='login.php'>Login</a>";
-                    
-                }
-                ?>
-        </nav>
-        <div id="cart-container">
-            <!-- cart icon image with link to cart page -->
-            <a href="cart.php">
-                <img id="cart-icon" src="Images/cart-no-bg.png" alt="Cart">
-                <span id="cart-count">0</span>
-            </a>
-        </div>
-    </header>
 
 
     <?php
@@ -108,7 +110,7 @@ $allOfTheProducts = fetchProducts();
     //The allOfTheProducts is an array of all the products  that  is going to be iterated through and the product is the current product that is being iterated through
     //Display each product
     foreach ($allOfTheProducts as $product) {
-        echo "<div>";
+        echo "<div class='product-container'>";
         echo "<img src='Images/{$product['image']}' alt='{$product['productName']}' width=50 height=50>";
         echo "<h2>{$product['productName']}</h2>";
         echo "<p>Price: {$product['price']}</p>";
