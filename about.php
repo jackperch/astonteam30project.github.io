c74ae6f63d0bf499021b2517a044af19067ae373
Jack Perchick <jack@remez.com>
2024-01-28 21:37:40 +0000

    updated about page

--- about.php
+++ about.php
@@ -50,40 +50,55 @@
 
 
 
-
-    <h1>About Us</h1>
-    <div class="left">
-        <h2 >Who are we?</h2>
-            <p>We are ACE GEAR SPORTS, your one and only destination for high-quality sports and workout equipment. 
-                At Ace Gear Sports, we are passionate about helping individuals pursue their fitness goals and enjoy 
-                an active lifestyle. Whether you're a seasoned athlete or just starting your fitness journey, we've 
-                got the gear you need to elevate your performance.</p>
+    <div class="content-container">
+        <div class="about-container">
+            <h1>About Us</h1>
+        </div>
+        <div class="about-content">
+            <div class="content">
+                <img src="Images/about-img-1.jpg" alt="content-image">
+                <h1>Who we are</h1>
+                <p>Welcome to ACE GEAR SPORTS, your premier destination for high-quality sports and workout 
+                    equipment. At Ace Gear Sports, our passion lies in helping individuals pursue their fitness 
+                    goals and embrace an active lifestyle. Whether you're a seasoned athlete or just starting, 
+                    we've got the gear to elevate your performance.
+                </p>
+            </div>
+            <div class="content">
+            <img src="Images/about-img-2.jpg" alt="content-image">
+                <h1>our mission</h1>
+                <p>Our mission at Ace Gear Sports is to provide a one-stop online shop for a diverse range of sports and 
+                    workout equipment, catering to the needs of fitness enthusiasts, athletes, and individuals seeking 
+                    top-notch gear for their active pursuits. We believe that a well-equipped athlete is a confident and 
+                    motivated one, and our curated selection reflects our commitment to quality and performance.
+                </p>
+            </div>
+            <div class="content">
+            <img src="Images/about-img-3.jpg" alt="content-image">
+                <h1>Why choose us</h1>
+                <p>At Ace Gear Sports, we prioritize Quality Assurance, handpicking each durable product. Our Diverse
+                     Selection caters to cardio enthusiasts, weightlifters, yoga practitioners, and team sports players.
+                      Benefit from Expert Advice; our fitness enthusiasts provide personalized recommendations. 
+                      Experience Convenience with Ace Gear Sports - shop top-notch equipment from home, with direct 
+                      shipping.
+                </p>
+                <!--<p>Quality Assurance: We understand the importance of reliable and durable equipment. That's why we 
+                    handpick each product, ensuring it meets our stringent quality standards. When you shop with Ace Gear 
+                    Sports, you can trust that you're investing in gear that stands the test of time.</p>
+                <p>Diverse Selection: From cardio enthusiasts to weightlifters, yoga practitioners to team sports players, 
+                    our inventory spans a wide range of activities. Explore our diverse selection of equipment and accessories 
+                    tailored to various sports and workout routines.</p>
+                <p>Expert Advice: Not sure which equipment is right for you? Our team of fitness enthusiasts is here to help. 
+                    Feel free to reach out for personalized recommendations and expert advice to make informed decisions about 
+                    your gear.</p>
+                <p>Convenience: Ace Gear Sports brings the store to your doorstep. Enjoy the convenience of browsing and 
+                    shopping for top-notch equipment from the comfort of your home. We ship directly to you, making the 
+                    process seamless and hassle-free.</p>-->
+            </div>
+        </div>    
+        <!--<h2>Soooooo what are you waiting for?</h2>
+        <h2>Get up and Gear up with ACE GEAR SPORTS!</h2>-->
     </div>
-    <div class="right">
-        <h2>Whats our mission?</h2>
-            <p>Our mission at Ace Gear Sports is to provide a one-stop online shop for a diverse range of sports and 
-                workout equipment, catering to the needs of fitness enthusiasts, athletes, and individuals seeking 
-                top-notch gear for their active pursuits. We believe that a well-equipped athlete is a confident and 
-                motivated one, and our curated selection reflects our commitment to quality and performance.</p>
-    </div>
-    <div class="left">
-        <h2>Why choose us?</h2>
-        <p>Quality Assurance: We understand the importance of reliable and durable equipment. That's why we 
-            handpick each product, ensuring it meets our stringent quality standards. When you shop with Ace Gear 
-            Sports, you can trust that you're investing in gear that stands the test of time.</p>
-        <p>Diverse Selection: From cardio enthusiasts to weightlifters, yoga practitioners to team sports players, 
-            our inventory spans a wide range of activities. Explore our diverse selection of equipment and accessories 
-            tailored to various sports and workout routines.</p>
-        <p>Expert Advice: Not sure which equipment is right for you? Our team of fitness enthusiasts is here to help. 
-            Feel free to reach out for personalized recommendations and expert advice to make informed decisions about 
-            your gear.</p>
-        <p>Convenience: Ace Gear Sports brings the store to your doorstep. Enjoy the convenience of browsing and 
-            shopping for top-notch equipment from the comfort of your home. We ship directly to you, making the 
-            process seamless and hassle-free.</p>
-    </div>    
-    <h2>Soooooo what are you waiting for?</h2>
-    <h2>Get up and Gear up with ACE GEAR SPORTS!</h2>
-
 
 
     <footer>
