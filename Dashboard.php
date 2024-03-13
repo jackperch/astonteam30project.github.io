024d9aff5b5cd14ca7f77722cc4cddba18261523
BigDrew <148326387+BigDrew01@users.noreply.github.com>
2024-03-13 21:47:19 +0000

    Update Dashboard.php

--- Dashboard.php
+++ Dashboard.php
@@ -23,14 +23,6 @@
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">ADMIN DASHBOARD</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             PRODUCTS
@@ -70,8 +62,6 @@
         <li class="nav-item">
           <a class="nav-link" href="adminLogout.php">Logout</a>
         </li>
-			    
-                    </ul>
                 </div>
             </div>
         </nav>
