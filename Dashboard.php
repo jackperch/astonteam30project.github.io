97e6932e31906bc12f178bb51e0de04f14400433
BigDrew <148326387+BigDrew01@users.noreply.github.com>
2024-03-13 20:27:18 +0000

    Update Dashboard.php

--- Dashboard.php
+++ Dashboard.php
@@ -1,113 +1,60 @@
-<?php
-session_start();
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>ACE GEAR</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="CSS/styles.css">
+        <link rel="stylesheet" href="CSS/index.css">
+        <style>
+            @import url('https://fonts.googleapis.com/css2?family=Sono&display=swap');
+        </style>
+        <!--<script src="/js/main.js"></script> -->
+    </head>
 
-if (isset($_SESSION['adminID'])) {
-	if (isset($_POST['btnEdit'])) {
-		$_SESSION['sesProductId'] = $_POST['btnEdit'];
-		header('location: ./editProduct.php');
-	}
-	if (isset($_POST['btnDel'])) {
-		require_once("connectionDB.php");
-		$sql = "DELETE FROM Product WHERE productId = ?";
-		$stmt = $conn->prepare($sql);
-		$stmt->bind_param('i', $productId);
-		$productId = $_POST['btnDel'];
-		$stmt->execute();
-		if ($stmt)
-			echo "<script>alert('Record Deleted Successfully!');window.location.replace('./Dashboard.php');</script>";
-		else
-			echo "<script>alert('Record not Deleted!');</script>";
-	}
-?>
 
-	<!DOCTYPE html>
-	<html>
+ <body>
+        <header>
+            <div id="logo-container">
+                <!-- logo image -->
+                <img id="logo" src="Images/Logo-no-bg.png" alt="Logo">
+                <h1 id="nav-bar-text">ADMIN DASHBOARD</h1>
+            </div>
+            <div id="search-container">
+                <input type="text" id="search-bar" placeholder="Search...">
+                <button id="search-button">Search</button>
+            </div>
+            <nav>
+                <a href="addProduct.php">Add Product</a>
+                <a href="adduser.php">Add User</a>
+                <a href="addAdmin.php">Add Admin</a>
+		<a href="editAdmins.php">Edit Admins</a>
+                <a href="addOrder.php">Add Order</a>
+		<a href="editOrders.php">Edit Orders</a>
+		<a href="editProducts.php">Edit Products</a>
+		<a href="editusers.php">Edit Users</a>
+                <?php 
+                session_start();
+                if (isset($_SESSION['adminID'])) {
+                    echo "<a href='adminLogout.php'>Logout</a>";
+                } else {
+                    echo "<a href='adminLogin.php'>Login</a>";
+                }
+                ?>
+            </nav>
+		
+		<footer>
+            <div class="footer-container">
+                <div class="footer-links">
+                    <a href="reviews.php">Reviews</a>
+                    <a href="contact.php">Contact Us</a>
+                    <a href="about.php">About Us</a>
+                    <a href="privacy-policy.php">Privacy Policy</a>
+                </div>
+            </div>
+        </footer>
+</body>
 
-	<head>
-		<meta charset="UTF-8">
-		<title>ACE GEAR ADMIN</title>
-		<!-- <link rel="stylesheet" href="../assets/style.css"> -->
-		<link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.css">
-		<style>
-			h1>a {
-				font-size: 25px;
-				float: right;
-				padding-right: 40px;
-				padding-top: 15px;
-			}
-		</style>
-	</head>
 
-	<body>
-		<SCRIPT type="text/javascript">
-			// var adr = '../attacker.php?victim_cookie=' + escape(document.cookie);
-			// alert(adr);
-			// alert(document.cookie);
-		</SCRIPT>
-		<h1>
-			&emsp;ACE GEAR - Dashboard
-			<a href="./adminLogout.php">Logout</a>
-			<a href="./addProduct.php">Add Product</a>
-		</h1>
-		<hr>
-		<form method="POST">
-			<?php
-  require_once("connectionDB.php");
-function fetchProducts() {
-}
-    global $db;
-
-    try {
-         // database connection code 
-			require_once("connectionDB.php");
-			$sql = "SELECT * FROM Product";
-			$result = mysqli_query($conn, $sql);
-
-			if ($result->num_rows < 1)
-				echo "<h4 align='center'>Product not exist!</h4>";
-			else {
-				$output = "";
-				$output = " <table align='center' cellpadding='20' cellspacing='0'>
-						<tr>
-							<th>Image</th>
-							<th>Product Id</th>
-							<th>Product Name</th>
-							<th>Price</th>
-							<th>Product Description</th>
-							<th>Colour</th>
-                                                        <th>Size</th>
-              				                <th>Category</th>
-				                        <th>Action</th>
-						</tr>";
-				while ($row = mysqli_fetch_array($result)) {
-					$output .= "<tr>
-									<td><img src='../images/" . $row['Image'] . "' width='150px' height='150px'></td>
-									<td>" . $row['productId'] . "</td>
-									<td>" . $row['product_name'] . "</td>
-                  					<td>" . $row['price'] . "</td>
-                  					<td>" . $row['description'] . "</td>
-									<td>" . $row['categoryID'] . "</td>
-									<td>" . $row['colour'] . "</td>
-                  					<td>" . $row['size'] . "</td>
-									<td>" . $row['stock'] . "</td>
-									<td>
-										<button type='submit' name='btnEdit' value='" . $row['productId'] . "'>Edit</button>&ensp;
-										<button type='submit' name='btnDel' value='" . $row['productId'] . "'>Delete</button>
-									</td>
-								</tr>";
-				}
-				$output .= "</table>";
-				echo $output;
-			}
-			?>
-		</form>
-	</body>
-
-	</html>
-
-<?php
-}
-} else {
-	echo "Please <a href='./adminLogin.php'>Login</a> first!";
-}
-?>
+</html>
+		
