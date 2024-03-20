8f7e6a4b79c0319b43556c7e223aa0be99283ee9
saad-13 <148326859+saad-13@users.noreply.github.com>
2024-03-20 15:28:08 +0000

    added contatc requests on admin dashboatrd

--- Dashboard.php
+++ Dashboard.php
@@ -208,6 +208,16 @@
                     </div>
                 </div>
             </div>
+            <div class="col-md-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="images/admin-icon.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Contact Requests</h5>
+                        <p class="card-text">View Contact Requests here:</p>
+                        <a href="contactRequests.php" class="btn btn-primary">Go to Requests</a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
                         
