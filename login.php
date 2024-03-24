d0b4566c9469c6e6c4b02709201d3361d57ea641
tharrm <131722887+tharrm@users.noreply.github.com>
2024-03-24 17:26:41 +0000

    making sure its  lower case so it works on the server

--- login.php
+++ login.php
@@ -25,7 +25,7 @@
     try {
       //Query DB to find the matching username/password
       //using prepare/bindparameter to prevent SQL injection.
-        $SQL = $db->prepare('SELECT password, CustomerID FROM Customers WHERE username = ?');
+        $SQL = $db->prepare('SELECT password, customerID FROM customers WHERE username = ?');
         $SQL->execute(array($_POST['username']));
           
         // fetch the result row and check 
